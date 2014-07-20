89891e2162b6ead9cfe75e8c29b896d582aafed1
ILAN <ilan_himself@hotmail.fr>
2014-07-20 20:15:49 +0200

    changes

--- django_map/urls.py
+++ django_map/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
 import map
 urlpatterns = patterns('',
+	url(r'^', include('map.urls')),
     url(r'^map/', include('map.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
